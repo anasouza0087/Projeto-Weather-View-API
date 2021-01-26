766384860d05dc721fcbb5a810bd497cacab6b56
anasouza0087 <anasouza0087@users.noreply.github.com>
2021-01-25 22:10:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const today = document.getElementById("currentDay")
+today.innerHTML = new Date()
+
+/*const greets = document.getElementById("greets")
+
+const currentDate = new Date();
+const hours = currentDate.getHours();
+
+//console.log(hours)
+
+//console.log(hours >= 21 || hours < 5 ? "Boa noite" : "Bom dia")
+
+//showGreets = hours => {
+function showHoras(hours) {
+    console.log(hours >= 21 || hours < 5 ? "Boa noite" : "Bom dia")
+        /*if (hours >= 5 || hours < 12) {
+            //return greets.innerHTML = "Bom dia!"
+            console.log('Bom dia')
+        }
+        if (hours >= 12 || hours < 17) {
+            //return greets.innerHTML = "Boa tarde!"
+            console.log('Boa tarde')
+        } else {
+            //return greets.innerHTML = "Boa noite!"
+            console.log('Boa noite')
+        }
+        //}
+}
+*/
