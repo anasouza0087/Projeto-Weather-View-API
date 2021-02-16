ac202fe2de5246347525d20ed66c5c26470f163d
anasouza0087 <anasouza0087@users.noreply.github.com>
2021-02-16 20:57:59 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
-const today = document.getElementById("currentDay")
-today.innerHTML = new Date()
-
-/*const greets = document.getElementById("greets")
-
+const greets = document.getElementById("greets")
 const currentDate = new Date();
 const hours = currentDate.getHours();
 
-//console.log(hours)
+let showHours = hours => {
+    if (hours >= 5 || hours < 12) {
+        return greets.innerText = "Bom Dia!"
 
-//console.log(hours >= 21 || hours < 5 ? "Boa noite" : "Bom dia")
+    } else if (hours >= 12 || hours < 18) {
+        return greets.innerText = "Boa Tarde!"
 
-//showGreets = hours => {
-function showHoras(hours) {
-    console.log(hours >= 21 || hours < 5 ? "Boa noite" : "Bom dia")
-        /*if (hours >= 5 || hours < 12) {
-            //return greets.innerHTML = "Bom dia!"
-            console.log('Bom dia')
-        }
-        if (hours >= 12 || hours < 17) {
-            //return greets.innerHTML = "Boa tarde!"
-            console.log('Boa tarde')
-        } else {
-            //return greets.innerHTML = "Boa noite!"
-            console.log('Boa noite')
-        }
-        //}
+    } else {
+        return greets.innerText = "Boa Noite!"
+
+    }
 }
-*/
+
+let showDate = () => {
+    let CompleteDate = new Date()
+    let month = CompleteDate.getDay()
+    let day = CompleteDate.getDate()
+    document.getElementById("currentDay").innerHTML = `${day} / ${month}`
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showHours()
+    showDate()
+})
+
+/*fetch('https://api.hgbrasil.com/weather?format=json-cors&key=a1db9024&city_name=Osasco,SP')
+    .then(res => res.json())
+    .then(data => console.log(data))*/
